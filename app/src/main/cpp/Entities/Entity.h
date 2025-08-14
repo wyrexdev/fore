70220927af4b78f932a1e7bb58e42279dc5b2e6e
wyrexdev <uchihasasukedev@gmail.com>
2025-08-14 17:12:06 +0300

    Entity Features and Shader Class

--- app/src/main/cpp/Entities/Entity.h
+++ app/src/main/cpp/Entities/Entity.h
@@ -5,17 +5,27 @@
 #ifndef FORE_ENTITY_H
 #define FORE_ENTITY_H
 
+#include <unordered_map>
+#include <vector>
+
 #include "Math/Vector3.h"
 #include "Graphics/Color.h"
+#include "Components/Component.h"
 
 class Entity {
 public:
     Entity();
 
+    int id;
+
     std::string title;
     std::string description;
 
     bool isVisible;
+
+    std::vector<float> vertices;
+
+    std::unordered_map<int, Component> components;
 
     Graphics::Color color = Graphics::Color("#FF8000");
 
---
+++ app/src/main/cpp/Graphics/Shader.h
@@ -0,0 +1,14 @@
+//
+// Created by wyrex on 8/14/25.
+//
+
+#ifndef FORE_SHADER_H
+#define FORE_SHADER_H
+
+
+class Shader {
+
+};
+
+
+#endif //FORE_SHADER_H
